8e53d2b03932f9fb3b6cf163805cefc101ee577c
Akram <m.akram.arar@gmail.com>
2020-03-29 05:27:00 +0200

    Contenu actuel

--- src/main/java/PGL/pglp_4_2/MoteurRPN.java
+++ src/main/java/PGL/pglp_4_2/MoteurRPN.java
@@ -6,18 +6,41 @@ public class MoteurRPN extends Interpreteur{
 	
 	public LinkedList<Double> pile;
 	
+	/**
+	 * Constructeur
+	 */
 	public MoteurRPN() {
 		pile = new LinkedList<Double>();
 	}
-
+	
+	/**
+	 * 
+	 * @param number num a empiler
+	 */
 	public void empilerInt(final double number) {
 		pile.push(number);
 	}
 	
+	/**
+	 * Appliquer une operation
+	 * @param oper operateur
+	 */
 	public void appliquerOperation(final Operation oper) {
 		double A= pile.pop();
 		double B= pile.pop();
 		double R = oper.eval(A,B);
 		this.empilerInt(R);
 	}
+	
+	/**
+	 * Retourner les operandes
+	 */
+    public void ContenuActuel() {
+        System.out.print("Affichage duContenu : [");
+        for (Double d : pile) {
+            System.out.print(" "+d + "=>");
+        }
+        System.out.print("] \n");
+    }
+
 }
